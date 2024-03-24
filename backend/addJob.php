93e0e73c027faad1baad54e833b4a6c0b33dda9d
H-M-1999 <81930506@students.liu.edu.lb>
2024-03-24 22:10:14 +0200

    finished the add job API

--- backend/addJob.php
+++ backend/addJob.php
@@ -3,3 +3,10 @@
 include 'connection.php';
 
 $company = $_POST['company'];
+$description = $_POST['description'];
+$applicants=0;
+
+
+$query = $mysqli->prepare('insert into jobs (company_id,description,applicants) values (?,?,?)');
+$query->bind_param('ssi',$company, $description,$applicants);
+$query->execute();
